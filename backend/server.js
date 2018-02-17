974f56d77e202cef8acd7fdd0640163dbbe584cd
wildlifehexagon <crossthebreeze@gmail.com>
2018-02-17 17:20:11 -0600

    set up backend base

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+import express from "express"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
+
+const app = express()
+app.use(bodyParser.json())
+
+app.listen(8080, () => console.log("Server is running on localhost:8080"))
