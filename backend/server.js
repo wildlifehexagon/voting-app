8978b213898160d95930ea7d2fc29d73704e6f1e
wildlifehexagon <crossthebreeze@gmail.com>
2018-02-18 13:01:53 -0600

    set up mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
+const cookieSession = require("cookie-session")
+const passport = require("passport")
+const keys = require("./config/keys")
+require("./models/User")
 require("./services/passport")
 
+mongoose.connect(keys.mongoURI)
+
 const app = express()
-app.use(bodyParser.json())
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
 require("./routes/authRoutes")(app)
 
 const PORT = process.env.PORT || 8080
-app.listen(PORT, () => console.log("Server is running on", PORT))
+app.listen(PORT, console.log("Listening on", PORT))
